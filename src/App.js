853033db7cb0521a3e753181a48f108b5c8ab438
bowershaley <156343634+bowershaley@users.noreply.github.com>
2024-03-12 16:10:08 +0000

    added HTML

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container m-0">
-        <h1>Weather App</h1>
+        <Weather />
         <footer>
           This website was coded by{" "}
           <a
