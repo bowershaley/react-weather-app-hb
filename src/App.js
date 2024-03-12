0677e8458a03787459d640129f3c3936e0d85ec0
bowershaley <156343634+bowershaley@users.noreply.github.com>
2024-03-12 15:33:12 +0000

    added github repo url to footer

--- src/App.js
+++ src/App.js
@@ -2,5 +2,28 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This website was coded by{" "}
+        <a
+          href="https://github.com/bowershaley"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Haley Bowers
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/bowershaley/react-weather-app-hb.git"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced
+        </a>
+        .
+      </footer>
+    </div>
+  );
 }
