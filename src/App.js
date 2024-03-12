1774c762896090d643da78db6f10dc90152b530e
bowershaley <156343634+bowershaley@users.noreply.github.com>
2024-03-12 15:55:53 +0000

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,26 +4,28 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This website was coded by{" "}
-        <a
-          href="https://github.com/bowershaley"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Haley Bowers
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/bowershaley/react-weather-app-hb.git"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced
-        </a>
-        .
-      </footer>
+      <div className="container m-0">
+        <h1>Weather App</h1>
+        <footer>
+          This website was coded by{" "}
+          <a
+            href="https://github.com/bowershaley"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Haley Bowers
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/bowershaley/react-weather-app-hb.git"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
